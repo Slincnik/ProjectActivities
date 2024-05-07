2b878ce0f8520da9c4e11b275d51d39cd36a455f
Slincnik <pattyshow21@mail.ru>
2024-05-08 00:57:07 +0300

    style: home and default layout style

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,5 +1,90 @@
 <template>
-  <h1>Home</h1>
+  <div class="flex flex-col max-w-[1210px] mx-auto">
+    <div
+      class="flex flex-col-reverse max-w-full xl:flex-row-reverse lg:flex-row-reverse md:flex-col-reverse sm:flex-col-reverse mb-4 pb-1 border-b-2 border-black xl:pb-0 lg:pb-0 md:pb-1 sm:pb-1 md:border-b-2 sm:border-b-2 xl:border-none lg:border-none"
+    >
+      <img
+        class="h-auto w-[300px] self-center rounded-md object-cover xl:h-auto xl:w-[600px] lg:h-full lg:w-[450px] md:h-auto md:w-auto sm:h-full sm:w-[500px]"
+        src="@/assets/img/background/first-card.jpg"
+        alt=""
+      />
+      <div class="flex flex-col justify-center">
+        <p
+          class="text-xl xl:text-3xl lg:text-3xl md:text-2xl sm:text-xl text-center xl:text-left lg:text-left md:text-center sm:text-center ml-5 mb-5 leading-9 tracking-wider text-pretty"
+        >
+          Присоединяйтесь к нам и узнайте, почему Table Quest - это не просто игры.
+        </p>
+        <p
+          class="text-xl xl:text-3xl lg:text-3xl md:text-2xl sm:text-xl text-center xl:text-left lg:text-left md:text-center sm:text-center ml-5 leading-9 tracking-wider text-pretty"
+        >
+          Это встречи, эмоции и незабываемые вечера, наполненные весельем и дружбой. Здесь каждый
+          находит свой кусочек игрового счастья! 🎲🚀
+        </p>
+      </div>
+    </div>
+    <div
+      class="flex flex-col max-w-full xl:flex-row lg:flex-row md:flex-col sm:flex-col mb-4 pb-1 border-b-2 border-black xl:pb-0 lg:pb-0 md:pb-1 sm:pb-1 md:border-b-2 sm:border-b-2 xl:border-none lg:border-none"
+    >
+      <img
+        class="h-auto w-[300px] self-center rounded-md object-cover xl:h-auto xl:w-[600px] lg:h-full lg:w-[450px] md:h-auto md:w-auto sm:h-full sm:w-[500px]"
+        src="@/assets/img/background/second-card.jpg"
+        alt=""
+      />
+      <div class="flex flex-col justify-center">
+        <p
+          class="text-xl xl:text-3xl lg:text-3xl md:text-2xl sm:text-xl text-center xl:text-left lg:text-left md:text-center sm:text-center ml-5 leading-9 tracking-wide"
+        >
+          🎲 Афиша Мероприятий:Погружайтесь в мир настольных игр с нашей подборкой захватывающих
+          событий. От турниров и чемпионатов до тематических игровых вечеров - здесь всегда что-то
+          происходит.
+        </p>
+      </div>
+    </div>
+    <div
+      class="flex flex-col-reverse max-w-full xl:flex-row-reverse lg:flex-row-reverse md:flex-col-reverse sm:flex-col-reverse mb-4 pb-1 border-b-2 border-black xl:pb-0 lg:pb-0 md:pb-1 sm:pb-1 md:border-b-2 sm:border-b-2 xl:border-none lg:border-none"
+    >
+      <img
+        class="h-auto w-[300px] self-center rounded-md object-cover xl:h-auto xl:w-[600px] lg:h-full lg:w-[450px] md:h-auto md:w-auto sm:h-full sm:w-[500px]"
+        src="@/assets/img/background/three-card.jpg"
+        alt=""
+      />
+      <div class="flex flex-col justify-center">
+        <p
+          class="text-xl xl:text-3xl lg:text-3xl md:text-2xl sm:text-xl text-center xl:text-left lg:text-left md:text-center sm:text-center mb-5 leading-9 tracking-wider text-balance"
+        >
+          🤝 Встречи и Общение:Здесь вы найдете идеальное место для обмена впечатлениями, обсуждения
+          стратегий и встречи единомышленников. Table Quest - это не просто мероприятия, а
+          возможность расширить свой круг общения
+        </p>
+        <p
+          class="text-xl xl:text-3xl lg:text-3xl md:text-2xl sm:text-xl text-center xl:text-left lg:text-left md:text-center sm:text-center leading-9 tracking-wider text-balance"
+        >
+          🎉 Эксклюзивные Премьеры:Мы первыми представляем вам самые ожидаемые настольные игры.
+          Будьте в курсе последних трендов и окунитесь в новые игровые миры.
+        </p>
+      </div>
+    </div>
+    <div class="flex flex-col max-w-full xl:flex-row lg:flex-row md:flex-col sm:flex-col">
+      <img
+        class="h-auto w-[300px] self-center rounded-md object-cover xl:h-auto xl:w-[600px] lg:h-full lg:w-[450px] md:h-auto md:w-auto sm:h-full sm:w-[500px]"
+        src="@/assets/img/background/four-card.jpg"
+        alt=""
+      />
+      <div class="flex flex-col justify-center">
+        <p
+          class="text-xl xl:text-3xl lg:text-3xl md:text-2xl sm:text-xl text-center xl:text-left lg:text-left md:text-center sm:text-center ml-5 mb-5 leading-9 tracking-wider text-pretty"
+        >
+          Присоединяйтесь к нам и узнайте, почему Table Quest - это не просто игры.
+        </p>
+        <p
+          class="text-xl xl:text-3xl lg:text-3xl md:text-2xl sm:text-xl text-center xl:text-left lg:text-left md:text-center sm:text-center ml-5 leading-9 tracking-wider text-pretty"
+        >
+          Это встречи, эмоции и незабываемые вечера, наполненные весельем и дружбой. Здесь каждый
+          находит свой кусочек игрового счастья! 🎲🚀
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
