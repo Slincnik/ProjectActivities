f27edfe6f2046d7ad1b59290f35a51084eca4f87
Slincnik <pattyshow21@mail.ru>
2024-05-09 18:03:53 +0300

    feat: auth logic in login page

--- src/pages/Auth/LoginPage.vue
+++ src/pages/Auth/LoginPage.vue
@@ -9,17 +9,28 @@
           <div class="grid items-center w-full gap-4">
             <div class="flex flex-col space-y-1.5">
               <Label for="name">Email</Label>
-              <Input v-model="DTO.email" id="name" placeholder="test@test.ru" />
+              <Input
+                :disabled="isPending"
+                v-model="DTO.email"
+                id="name"
+                placeholder="test@test.ru"
+              />
             </div>
             <div class="flex flex-col space-y-1.5">
               <Label for="password">Пароль</Label>
-              <Input v-model="DTO.password" id="password" type="password" placeholder="Password" />
+              <Input
+                :disabled="isPending"
+                v-model="DTO.password"
+                id="password"
+                type="password"
+                placeholder="Password"
+              />
             </div>
           </div>
         </form>
       </CardContent>
       <CardFooter class="flex justify-end px-6 pb-6">
-        <Button>Войти</Button>
+        <Button :disabled="isPending" @click="mutate">Войти</Button>
       </CardFooter>
     </Card>
   </div>
@@ -31,10 +42,13 @@ import { Button } from '@/components/ui/button'
 import { Card, CardHeader, CardTitle, CardContent, CardFooter } from '@/components/ui/card'
 import { Label } from '@/components/ui/label'
 import { Input } from '@/components/ui/input'
+import { useLogin } from '@/api/auth'
 
 const DTO = shallowReactive({
   email: '',
   password: ''
 })
+
+const { isPending, mutate } = useLogin(DTO)
 </script>
 
