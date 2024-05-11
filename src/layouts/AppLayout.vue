0086205ea9c47c96ab4a6aad624c76abb9de2ec3
Slincnik <pattyshow21@mail.ru>
2024-05-11 16:47:19 +0300

    feat: put a spinner if the page is not loaded

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,18 +1,30 @@
 <template>
-  <component :is="route.meta.layoutComponent">
-    <slot />
-  </component>
-  <footer>
-    <div class="container border-t mt-4 dark:border-white border-black max-w-[1100px] mx-auto">
-      <p class="text-center py-[10px] footer_desc">Copyright © 2024</p>
-    </div>
-  </footer>
+  <div v-if="!isRouterReady" class="flex justify-center items-center h-dvh">
+    <Spinner size="36" />
+  </div>
+  <template v-else>
+    <component :is="route.meta.layoutComponent">
+      <slot />
+    </component>
+    <footer>
+      <div class="container border-t mt-4 dark:border-white border-black max-w-[1100px] mx-auto">
+        <p class="text-center py-[10px] footer_desc">Copyright © 2024</p>
+      </div>
+    </footer>
+  </template>
 </template>
 
 <script setup lang="ts">
-import { useRoute } from 'vue-router'
+import { ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { Spinner } from '@/components/ui/spinner'
+
+const isRouterReady = ref(false)
 
 const route = useRoute()
+const router = useRouter()
+
+router.isReady().finally(() => (isRouterReady.value = true))
 </script>
 
 <style scoped>
